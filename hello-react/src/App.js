37ab084ea56efbb67534b06409d016e4c3641671
lijing18 <lijing18@360.cn>
2021-02-18 18:37:06 +0800

    210218-事件监听、this指向、setState的参数及与render的关系

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,14 +1,35 @@
+import React, { Component, Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+class Title extends React.Component {
+    // 传入自定义参数并接收
+    handleOnClick(param, e) {
+        console.log('click', e.target);
+        console.log('this', this)
+        console.log('param', param)
+    }
+    render() {
+        console.log('render-this', this)
+        return (
+            <h1 onClick={this.handleOnClick.bind(this, 'abc')}>这里是H1</h1>
+        )
+    }
+}
+
+
+
+
 
 function App() {
     return (
         <div className="App">
+            <Title ></Title>
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
                     hello react
-        </p>
+                </p>
                 <a
                     className="App-link"
                     href="https://reactjs.org"
@@ -16,7 +37,7 @@ function App() {
                     rel="noopener noreferrer"
                 >
                     Learn React
-        </a>
+                </a>
             </header>
         </div>
     );
